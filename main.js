74ffcce02121a56397317e34319a9173ac3049d0
sharquan4 <tiffanylashspa@gmail.com>
2021-04-09 13:02:17 -0700

    testing

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const key = '5f9c7d0867fdc0ff0c7a664c0561d7bd';
 const div = document.querySelector('.slide-1');
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('search');
 
+searchForm.addEventListener('click', (e) => {
+    e.preventDefault;
+    const searchValue = e.target.document.querySelector(searchInput).value;
+    fetchAPI(searchValue);
+})
+
+const fetchAPI = (searchValue) => {
+    fetch(`http://api.weatherstack.com/current?access_key=${key}&query=${searchValue}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
