dbc908f8f5c1cb4270ff7582d38f83e2962faaa9
sharquan4 <tiffanylashspa@gmail.com>
2021-04-09 18:16:04 -0700

    Importing project

--- images/README.md
+++ images/README.md
@@ -1,2 +1,3 @@
 images and gifs sources:
 https://www.washington.edu/cec/f-toc.html
+https://www.washington.edu/brand/graphic-elements/logo-guidelines/
--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-const btn = document.querySelector('.btn');
-const test = document.querySelector('.test');
-
-btn.addEventListener('click', () => {
-    test.style.display = 'block';
-})
 
