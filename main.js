7abd52b5f40a6b5316c475325b3e1c452c37e114
sharquan4 <tiffanylashspa@gmail.com>
2021-04-10 14:17:03 -0700

    Importing project

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 //search
 const input = document.querySelector('.search');
-const item = document.querySelector('.here');
 let searchQuery = "";
 
 input.addEventListener('submit', (e) => {
     e.preventDefault;
-    searchQuery = e.target.search;
-
+    searchQuery = e.target.search.value;
+    document.location.href = '#testing';
 })
