fcb3dc232234f7dae8ff06082afe18fcfcf8a332
sharquan4 <tiffanylashspa@gmail.com>
2021-04-10 17:43:00 -0700

    micro change

--- import_test.js
+++ import_test.js
@@ -7,21 +7,21 @@ let box = document.querySelector('.box');
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch(`data/${teacherAndClass}.json`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        console.log("------------")
-        data.map( value => {
-            console.log(value[0])
-            box.innerHTML += 
-            `
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            console.log("------------")
+            data.map(value => {
+                console.log(value[0])
+                box.innerHTML +=
+                    `
             <p>${value[0]}</p>
             <p>${value[1]}</p>
             <p>${value[2]}</p>
             <p>${value[3]}</p>
             <p>${value[4]}</p>
             `
+            })
         })
-    })
 })
 
